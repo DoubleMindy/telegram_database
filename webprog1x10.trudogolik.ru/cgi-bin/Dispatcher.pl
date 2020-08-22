f94e65ade68de5ecdfbd20aa410176cbdc33276c
Ilya TImokhin <tim-star@ya.ru>
2020-08-22 16:03:14 +0300

    Added handler for only-one group_id deletion

--- webprog1x10.trudogolik.ru/cgi-bin/Dispatcher.pl
+++ webprog1x10.trudogolik.ru/cgi-bin/Dispatcher.pl
@@ -12,7 +12,7 @@ eval
   my $class = $io_cgi->param('class');
   my $event = $io_cgi->param('event');
 
-  my $group_id    = $io_cgi->param('group_id*');
+  my $group_id    = $io_cgi->param('group_id');
   my $group_title = $io_cgi->param('group_title');
   eval "require $class";
   my $obj = $class->new( $group_id, $group_title );
--- webprog1x10.trudogolik.ru/cgi-bin/Timokhin_group.pm
+++ webprog1x10.trudogolik.ru/cgi-bin/Timokhin_group.pm
@@ -36,9 +36,18 @@ package Timokhin_group;
   sub delete_row
   {
     my ($this) = @_;    
-    print "DUMPER: $this->{group_id}\n";
-
-    $group->db_delete( $this->{group_id} );
+    if ( ref($this->{group_id}) ne "ARRAY"  )
+    {
+      $group->db_delete( $this->{group_id} );
+    }
+    else
+    {
+      my @group_ids = @{ $this->{group_id} };
+      foreach my $gr ( @group_ids )
+      {
+        $group->db_delete( $gr );
+      }
+    }
     show_all();
   }
 
